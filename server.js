d12f36de20dacb3823706407cef4209a45f8bb7f
LintBin <312763889@qq.com>
2017-01-08 13:42:01 +0800

    修正下命名

--- server.js
+++ server.js
@@ -9,9 +9,23 @@ var common = require("./proxy_lib/commonUtil");
 var method = require("./proxy_lib/method");
 
 
-for(var i in config){
 
-	var serverConfig = config[i];
+var totalProxy = config.totalProxy;
+if(JSON.stringify(totalProxy) != "{}"){ 
+	http.createServer(function(req, proxyRes){
+
+		var hostname = req.headers.host;
+
+		var id = totalProxy[hostname];
+
+		
+	}).listen(80);
+}
+
+var serverConfigList = config.proxy;
+for(var i in serverConfigList){
+
+	var serverConfig = serverConfigList[i];
 
 	initServer(serverConfig);
 
@@ -24,129 +38,40 @@ for(var i in config){
 function initServer(serverConfig){
 	http.createServer(function(req, proxyRes){
 
-	var requestUrl = req.url;
+		var requestUrl = req.url;
 
-	var transferUrls = serverConfig.transferUrl;
+		var transferUrls = serverConfig.transferUrl;
 
-	var flag = false;
+		var flag = false;
 
-	//配置了以XX开头的则去请求服务器
-	for(var i=0;i<transferUrls.length;i++){
-		var transferUrl = transferUrls[i];
+		//配置了以XX开头的则去请求服务器
+		for(var i=0;i<transferUrls.length;i++){
+			var transferUrl = transferUrls[i];
 
-		if(common.startWith(requestUrl,transferUrl)){
-			flag = true;
-			break;
-		}
-	}
-
-	var localFileObj = serverConfig.localFile;
-
-
-
-	//处理是否指定url的代理
-	var temp ;
-	if(common.startWith(requestUrl,'/')){
-		temp = requestUrl.substring(1,requestUrl.length);
-	}
-
-	var fileUrl = localFileObj[temp];
-
-	if(fileUrl != undefined ){
-		requestUrl = "/" + fileUrl;
-		flag = false;
-	}
-
-	
-
-	//如果需要从服务器请求数据
-	if(flag){
-		var options = {
-			hostname: serverConfig.remoteServer,
-			port: serverConfig.remotePort,
-			path: requestUrl,
-			headers:req.headers
-		}
-
-		var methodStr = req.method.toUpperCase();
-
-		if(methodStr == "POST"){
-
-			req.on('data',function(data){
-
-				var contentType = req.headers['content-type'];
-
-				var postData;
-				if(common.startWith(contentType,"multipart/form-data")){
-				
-					postData = data;
-
-				}else{
-
-					postData = decodeURIComponent(data);
-
-				}
-
-				options.method = "POST";
-
-				method.post(options,postData,proxyRes);
-				
-			});
-			return ;
-		}
-
-		if(methodStr == "GET"){
-			options.method = "GET";
-
-			//是否要忽略掉"?"
-			if(serverConfig.ignoreQuestionMark){
-				if(requestUrl.indexOf(".html") != -1 || requestUrl.indexOf(".js") || requestUrl.indexOf(".css") ){
-					var questionMarkIndex = requestUrl.indexOf("?");
-					if(questionMarkIndex != -1){
-
-						requestUrl = requestUrl.substring(0,questionMarkIndex - 1);
-
-					}
-				}
+			if(common.startWith(requestUrl,transferUrl)){
+				flag = true;
+				break;
 			}
-
-			method.get(options,proxyRes);
-
 		}
 
-	    return ;
-	}
+		var localFileObj = serverConfig.localFile;
 
-	//从本地取数据
-	//requestUrl = requestUrl.slice(1,requestUrl.length);
-	var local_workspace_path = serverConfig.workspace;
-	
-	if(local_workspace_path != "" && local_workspace_path != undefined){
 
-		console.log(local_workspace_path);
-
-		if(common.endWith(local_workspace_path,"/")){
-
-			local_workspace_path = local_workspace_path.slice(0,local_workspace_path.length-1);
+		//处理是否指定url的代理
+		var temp ;
+		if(common.startWith(requestUrl,'/')){
+			temp = requestUrl.substring(1,requestUrl.length);
 		}
 
-		console.log(local_workspace_path);
+		var fileUrl = localFileObj[temp];
 
-		local_request_uri = local_workspace_path + requestUrl;
+		if(fileUrl != undefined ){
+			requestUrl = "/" + fileUrl;
+			flag = false;
+		}
 
-	}else{
-
-		local_request_uri = requestUrl;
-	}
-
-	console.log(local_request_uri);
-
-	fs.readFile(local_request_uri ,function readFileCallback(err, data){
-		//如果在本地找不到则到服务器端去找
-		if (err){
-
-			console.log("404 for this page :" + requestUrl);
-
+		//如果需要从服务器请求数据
+		if(flag){
 			var options = {
 				hostname: serverConfig.remoteServer,
 				port: serverConfig.remotePort,
@@ -154,33 +79,115 @@ function initServer(serverConfig){
 				headers:req.headers
 			}
 
-			method.get(options,proxyRes,serverConfig);
+			var methodStr = req.method.toUpperCase();
 
-	    }else{
+			if(methodStr == "POST"){
 
-	    	//在本地找静态资源
-	    	var responseHeader = {
-	    		"Server":"Apache-Coyote/1.1",
-	    	}
+				req.on('data',function(data){
 
-	    	if(common.endWith(requestUrl,".html")){
-	    		responseHeader['Content-Type'] = "text/html;charset=UTF8";
-	    	}
+					var contentType = req.headers['content-type'];
 
-	    	if(common.startWith(requestUrl,".css")){
-	    		responseHeader['Content-Type'] = "css/html;charset=UTF8";
-	    	}
-	    	
-	    	responseHeader['Content-Length'] = data.length;
+					var postData;
+					if(common.startWith(contentType,"multipart/form-data")){
+					
+						postData = data;
 
-	        proxyRes.writeHead(200, responseHeader);
-	        proxyRes.write(data);
+					}else{
 
-	        console.log("local file : " + local_request_uri);
-	        proxyRes.end();
+						postData = decodeURIComponent(data);
 
+					}
+
+					options.method = "POST";
+
+					method.post(options,postData,proxyRes);
+					
+				});
+				return ;
+			}
+
+			if(methodStr == "GET"){
+				options.method = "GET";
+
+				//是否要忽略掉"?"
+				if(serverConfig.ignoreQuestionMark){
+					if(requestUrl.indexOf(".html") != -1 || requestUrl.indexOf(".js") || requestUrl.indexOf(".css") ){
+						var questionMarkIndex = requestUrl.indexOf("?");
+						if(questionMarkIndex != -1){
+
+							requestUrl = requestUrl.substring(0,questionMarkIndex - 1);
+
+						}
+					}
+				}
+
+				method.get(options,proxyRes);
+
+			}
+
+		    return ;
 		}
-	});
+
+		//从本地取数据
+		var localWorkspacePath = serverConfig.workspace;
+
+		var localRequestUri ;
+		
+		if(localWorkspacePath != "" && localWorkspacePath != undefined){
+
+			if(common.endWith(localWorkspacePath,"/")){
+
+				localWorkspacePath = localWorkspacePath.slice(0,localWorkspacePath.length-1);
+			}
+
+			localRequestUri = localWorkspacePath + requestUrl;
+
+		}else{
+
+			localRequestUri = requestUrl;
+		}
+
+
+		fs.readFile(localRequestUri ,function (err, data){
+			//如果在本地找不到则到服务器端去找
+			if (err){
+
+				console.log("404 for this page :" + requestUrl);
+
+				var options = {
+					hostname: serverConfig.remoteServer,
+					port: serverConfig.remotePort,
+					path: requestUrl,
+					headers:req.headers
+				}
+
+				method.get(options,proxyRes,serverConfig);
+
+		    }else{
+
+		    	//在本地找静态资源
+		    	var responseHeader = {
+		    		"Server":"Apache-Coyote/1.1",
+		    	}
+
+		    	if(common.endWith(requestUrl,".html")){
+		    		responseHeader['Content-Type'] = "text/html;charset=UTF8";
+		    	}
+
+		    	if(common.startWith(requestUrl,".css")){
+		    		responseHeader['Content-Type'] = "css/html;charset=UTF8";
+		    	}
+		    	
+		    	responseHeader['Content-Length'] = data.length;
+
+		        proxyRes.writeHead(200, responseHeader);
+		        proxyRes.write(data);
+
+		        console.log("local file : " + localRequestUri);
+		        proxyRes.end();
+
+			}
+		});
 
 
 
